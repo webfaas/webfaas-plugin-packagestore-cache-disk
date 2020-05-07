a8b13c63b652986eb64672c79133c918b7e6bdd7
rogerio moreira <rogeriodegoiania@gmail.com>
2020-05-07 20:04:12 -0300

    refactor: support core 0.12.0

--- src/test/plugin.test.ts
+++ src/test/plugin.test.ts
@@ -28,7 +28,10 @@ describe("Plugin", () => {
                 }
             }
         }
-        let core2 = new Core( new Config(configData) );
+        let config = new Config();
+        config.read(configData);
+        let core2 = new Core( config );
+
         let plugin2 = new WebFassPlugin(core2);
         core2.getLog().changeCurrentLevel(LogLevelEnum.OFF);
         chai.expect(core2.getPackageStoreManager().getCache()).to.not.null;
